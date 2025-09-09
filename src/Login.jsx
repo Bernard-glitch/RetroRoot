a6481ea2ff03e94f9f9cb4c49e3831357c97b7dd
Bernard <twistor75@gmail.com>
2025-09-09 15:51:55 +0800

    small box remove (login page)

--- src/Login.jsx
+++ src/Login.jsx
@@ -345,18 +345,7 @@ function Login() {
                     </div>
                 )}
 
-                {/* Instructions for implementation */}
-                <div style={{
-                    marginTop: "2rem",
-                    padding: "1rem",
-                    backgroundColor: `${vintageTheme.colors.gold}20`,
-                    borderRadius: "8px",
-                    border: `1px solid ${vintageTheme.colors.gold}`,
-                    fontSize: "0.85rem",
-                    color: vintageTheme.colors.darkBrown
-                }}>
-                    <strong>Implementation Note:</strong>You can't login without signing up first.
-                </div>
+
             </div>
         </div>
     );
